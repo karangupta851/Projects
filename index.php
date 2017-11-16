eb8474124479d33d85b61408ac32a0e23eafcdef
Karan Gupta <25882557+karangupta851@users.noreply.github.com>
2017-11-16 13:03:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>
+    Application form
+    </title>
+  </head>
+  <body>
+    <div class ="container">
+      <div class="main">
+        <form action="" method="post" onsubmit="validate()">
+          <table>
+            <tr>
+              <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
+              <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
+            </tr>
+            <tr>
+              <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
+              <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
+            <tr>
+          </table>
+        </form>
+      </div>
+    <div>
+  <body>
+</html>

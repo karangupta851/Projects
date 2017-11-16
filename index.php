a94cf696fd567cddd1b4fc045e7fbebc520072d1
Karan Gupta <25882557+karangupta851@users.noreply.github.com>
2017-11-16 13:16:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <tr>
                <tr>
               <td><input type="radio" name="male" value="male">Male<input type="radio" name="female" value="female">FeMale</td>
-              <td></td>
+              <td><input type="text" name="phno" id="phno" placeholder="Phone Number"></td>
             </tr>
             <tr>
               <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>

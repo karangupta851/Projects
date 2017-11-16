13d5764f4fe2dcc4af45eccfe10bf17a8c799e8e
Karan Gupta <25882557+karangupta851@users.noreply.github.com>
2017-11-16 13:10:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,14 @@
               <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
             </tr>
             <tr>
+              <td><input type="email" name="email" id="email" placeholder="Email"></td>
+              <td><input type="date" name="dob" id="dob" placeholder="Date Of Birth"></td>
+            <tr>
+               <tr>
+              <td><input type="checkbox" name="male" value="male">Male</td>
+              <td><input type="checkbox" name="female" value="female">FeMale</td>
+            </tr>
+            <tr>
               <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
               <td><input type="text" name="fname" id="fname" placeholder="First Name"></td>
             <tr>
